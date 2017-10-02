f05b309cc36b6cf29de3b15016c2abfec35b4392
chenkaiyuan <chenkaiyuan91@gmail.com>
2017-10-02 18:11:23 +0800

    change sqlites datas
    upgrade 2017-10-02 18:11:20

--- myblog/article/admin.py
+++ myblog/article/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from article.models import Article
+#from .models import Post, Category, Tag
 
 import sys
 reload(sys)
@@ -8,3 +9,7 @@ reload(sys)
 admin.site.register(Article)
 
 sys.setdefaultencoding('utf8')
+
+#admin.site.register(Post)
+#admin.site.register(Category)
+#admin.site.register(Tag)
---
+++ myblog/article/migrations/0002_auto_20171002_1757.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('article', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=100)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=70)),
+                ('body', models.TextField()),
+                ('created_time', models.DateTimeField()),
+                ('modified_time', models.DateTimeField()),
+                ('excerpt', models.CharField(max_length=200, blank=True)),
+                ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+                ('category', models.ForeignKey(to='article.Category')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Tag',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=100)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='post',
+            name='tags',
+            field=models.ManyToManyField(to='article.Tag', blank=True),
+            preserve_default=True,
+        ),
+        migrations.AlterModelOptions(
+            name='article',
+            options={'ordering': ['-date_time']},
+        ),
+    ]
